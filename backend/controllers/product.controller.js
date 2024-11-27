d7a70d5977ab1d9e975d1c04e022c0b6f349dd22
vgdhieu0602 <vg.dhieu0602@gmail.com>
2024-11-28 01:28:24 +0700

    finish toggleFeatured

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -125,3 +125,32 @@ export const getProductsByCategory = async (req, res) => {
         res.status(500).json({message: "Server error: ", error: error.message })
     }
 }
+
+export const toggleFeaturedProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        
+        if(product) {
+            product.isFeatured =!product.isFeatured
+            await product.save()
+            await updateFeaturedProductCache()
+
+            res.json(updatedProduct)
+        } else {
+            res.status(404).json({ message: "Product not found" })
+        }
+    } catch (error) {
+        console.log("Error in toggleFeaturedProduct: ", error.message)
+        res.status(500).json({message: "Server error: ", error: error.message })
+    }
+}
+
+async function updateFeaturedProductCache () {
+    try {
+        const featuredProducts = await Product.find({isFeatured: true}).lean()
+        await redis.set('featured_products', JSON.stringify(featuredProducts))
+    } catch (error) {
+        console.log("Error in updateFeaturedProductCache: ", error.message)
+        res.status(500).json({message: "Server error: ", error: error.message })
+    }
+}

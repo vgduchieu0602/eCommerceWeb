13a082f9d3a314f7848de0e3056c4c2f13fc0e01
vgdhieu0602 <vg.dhieu0602@gmail.com>
2024-11-27 23:26:38 +0700

    finish getProductsByCategory

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -114,3 +114,14 @@ export const getRecommendedProducts = async (req, res) => {
         res.status(500).json({message: "Server error: ", error: error.message })
     }
 }
+
+export const getProductsByCategory = async (req, res) => {
+    const {category} = req.params
+    try {
+        const products = await Product.find({category})
+        res.json({ products })
+    } catch (error) {
+        console.log("Error in getRecommendedProductsController: ", error.message)
+        res.status(500).json({message: "Server error: ", error: error.message })
+    }
+}
